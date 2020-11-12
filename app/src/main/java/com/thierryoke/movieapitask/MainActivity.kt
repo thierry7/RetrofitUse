48435bb4918481da1cde2a11dceea9f40b3a64e5
fieldemployee <thierry7>
2020-11-12 13:15:10 -0500

    First commit

---
+++ app/src/main/java/com/thierryoke/movieapitask/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thierryoke.movieapitask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
